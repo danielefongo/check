288ead13fa9ca7b7a537f39769146bd245683b10
danielefongo <danielefongo@gmail.com>
2020-04-20 19:04:52 +0200

    first commit

---
+++ action.js
@@ -0,0 +1,32 @@
+module.exports = function(loader, toggl, timeSlotter, asker, config) {
+
+  this.run = async () => {
+    const moment = loader.load('moment')
+
+    const granularity = await chooseGranularity(asker)
+    console.log(granularity)
+
+    const start = moment().startOf(granularity)
+    const end = moment().endOf(granularity)
+    const projects = await toggl.getAllProjects()
+
+    toggl.getTimeEntries(start, end).then(entries => {
+      entries.forEach(entry => {
+        const project = projects.filter(project => project.id === entry.pid)[0]
+        print(project, entry.slot)
+      })
+    })
+  }
+
+  this.help = () => {
+    return "show inserted entries"
+  }
+}
+
+function print(project, slot) {
+  console.log(slot.description + ' on "' + project.description + '"')
+}
+
+async function chooseGranularity(asker) {
+  return asker.inquire('Select granularity', 'list', ['day', 'week', 'month', 'year'])
+}
